c13e615c63de5000b39de5f21d1027443d27c383
Karan Singh <karan singh>
2020-09-24 19:14:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import UserApi from "./userapi";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <UserApi />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/userapi.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function UserApi() {
+  const [name, setName] = useState();
+  const [profile, setProfile] = useState("");
+  const handleChange = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+  };
+
+  const SubmitHandler = (e) => {
+    e.preventDefault();
+    const url = `https://api.github.com/users/${name}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setProfile(data));
+  };
+
+  const imgUrl = profile.avatar_url;
+  return (
+    <div>
+      {profile.length === 0 ? <h4>Find your Github profile</h4> : ""}
+      {profile.length === 0 ? (
+        ""
+      ) : (
+        <div>
+          <img
+            style={{ width: 200, height: 200 }}
+            src={imgUrl}
+            alt="profile"
+          ></img>
+          <h1>"{profile.login}"</h1>
+          <h2>Follower : {profile.followers}</h2>
+          <h2>Following : {profile.following}</h2>
+        </div>
+      )}
+
+      <form onSubmit={SubmitHandler}>
+        <input
+          type="text"
+          placeholder="Enter your Github username"
+          style={{ padding: 12, width: 400, height: 30, fontSize: 20 }}
+          onChange={handleChange}
+        ></input>
+        <button style={{ padding: 15, fontSize: 20 }}>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default UserApi;
